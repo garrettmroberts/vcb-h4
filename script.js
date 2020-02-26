afdc5fe588aaef76b216f1434f004d6d5fde951e
Garrett Roberts <garrettmroberts@gmail.com>
2020-02-25 19:53:56 -0600

    added landing UI from which to start the quiz

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 var timer = 50;
 var score = 0;
+var questions = ["Which operator separates statements within a for loop?", "How does one prevent bubbling of an event?"];
+var answers = [":", ",", ";", "!", "preventDefault()", "stopPropogation()", "getItem()", "setItem()"];
 
 function updateUI() {
+  changeForms();
   for (var i = 0; i < questions.length; i++) {
     buildQuestion(i);
   };
 }
 
+function changeForms() {
+  $("#startDiv").addClass("d-none").removeClass("d-flex").removeClass("flex-column");
+  $(".formRow").removeClass("d-none");
+};
+
 function buildQuestion(index) {
   // Question and answer arrays.  Used to generate questions via indices
-  var questions = ["Which operator separates statements within a for loop?", "How does one prevent bubbling from an event?"];
-  var answers = [":", ",", ";", "!", "preventDefault()", "stopPropogation()", "getItem()", "setItem()"];
 
   // Sets Form Question
   $("#formQuestion").text(questions[index]).attr("for", "question" + (index + 1));
@@ -27,6 +33,6 @@ function buildQuestion(index) {
   $("#button2").text(answers[answerIndex + 1]);
   $("#button3").text(answers[answerIndex + 2]);
   $("#button4").text(answers[answerIndex + 3]);
-  
-}
-buildQuestion(1);
+};
+
+updateUI();
